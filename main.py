3eb608eb13822b68f5ee1e63aa865a875269fd83
syakekichi <nagai.ryoma@gmail.com>
2026-03-10 01:05:07 +0900

    add texture of the earth

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from skyfield.api import EarthSatellite, load
 from mpl_toolkits.mplot3d import Axes3D
+from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 
 print("program started")
+
+
+# 地球テクスチャ読み込み
+texture = Image.open("earth_texture.jpg")
+texture = texture.resize((512,256))
+texture = np.array(texture) / 255
+texture = np.flipud(texture)
 
 # ISS TLE
 line1 = "1 25544U 98067A   24060.54791667  .00016717  00000+0  10270-3 0  9001"
@@ -22,23 +30,40 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 # -------- 地球を描く --------
-
 earth_radius = 6371
 
-u = np.linspace(0, 2*np.pi, 100)
-v = np.linspace(0, np.pi, 100)
+h, w, _ = texture.shape
 
-earth_x = earth_radius * np.outer(np.cos(u), np.sin(v))
-earth_y = earth_radius * np.outer(np.sin(u), np.sin(v))
-earth_z = earth_radius * np.outer(np.ones(np.size(u)), np.cos(v))
+u = np.linspace(0, 2*np.pi, w)
+v = np.linspace(0, np.pi, h)
 
-ax.plot_surface(earth_x, earth_y, earth_z, color='blue', alpha=0.4)
+u, v = np.meshgrid(u, v)
+
+# ISS位置
+geocentric = satellite.at(times)
+sat_x, sat_y, sat_z = geocentric.position.km
+
+earth_x = earth_radius * np.cos(u) * np.sin(v)
+earth_y = earth_radius * np.sin(u) * np.sin(v)
+earth_z = earth_radius * np.cos(v)
+
+
+ax.plot_surface(
+    earth_x,
+    earth_y,
+    earth_z,
+    facecolors=texture,
+    rstride=4,
+    cstride=4
+)
 
 # -------- ISS軌道 --------
 
-ax.scatter(x[0], y[0], z[0], color='yellow', s=100)
+ax.scatter(x[0], y[0], z[0], color='yellow', s=200)
+ax.plot(x, y, z, color='red', linewidth=2)
 
 # 軸スケール
 ax.set_box_aspect([1,1,1])
+
 
 plt.show()
